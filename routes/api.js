a0c341038503657e84a90f6ab675179680b1fe1a
Aziza Baccouri <azizabaccouri@gmail.com>
2023-08-08 13:36:47 +0200

    last saving

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ import  express  from 'express';
 import Player from '../model/player.js'
 const router = express.Router();
 
+// get all players
 router.get('/players', async (req, res) => {
 try {
     const players = await Player.find({});
@@ -12,5 +13,19 @@ try {
     res.status(500).send('Server error');
 }
 });
+
+// get player by name 
+// example : http://localhost:3000/api/players/Kylian%20Mbapp%C3%A9
+router.get('/players/:name', async (req, res) => {
+    try {
+        const players = await Player.find({name : req.params.name});
+        console.log(players);
+        res.json(players);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Server error');
+    }
+    });
+    
 
 export default router;
--- scramping/scramper.js
+++ scramping/scramper.js
@@ -23,14 +23,14 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
             const nationality = $(element).find('td[data-title="Nationality"] > a').attr('title');
             const club = $(element).find('td[data-title="Team"] > a').attr('title');
             const overallRating = $(element).find('td[data-title="OVR / POT"] > span:nth-child(1)').text();
-            insertionPromises.push(collection.insertOne({ name, nationality, club, overallRating }));
-        });
+/*             insertionPromises.push(collection.insertOne({ name, nationality, club, overallRating }));
+ */        });
 
-        await Promise.all(insertionPromises);
+/*         await Promise.all(insertionPromises);
         console.log('Data insertion completed.');
         const docs = await collection.find({}).toArray();
-        console.log(`Found ${docs.length} documents in the collection`);
-        console.log(docs);
+        console.log(`Found ${docs.length} documents in the collection`); */
+   /*      console.log(docs); */
     } catch (error) {
         console.error('An error occurred:', error);
     } finally {
